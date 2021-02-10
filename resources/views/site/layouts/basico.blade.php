a85dde1d32cb197439916bc04076ceddd164fc01
chico3434 <chicokarkow@hotmail.com>
2021-02-09 22:54:00 -0300

    extends usado para criar template

---
+++ resources/views/site/layouts/basico.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/estilo.css') }}">
+    </head>
+    <body>
+        @yield('conteudo')
+    </body>
+</html>
