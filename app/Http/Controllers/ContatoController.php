8c4ea63af49540beeebdaf0b4ae8cc1326f78db1
chico3434 <chicokarkow@hotmail.com>
2021-02-12 17:24:14 -0300

    Form contato atualizado

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller {
-    public function contato() {
-        var_dump($_POST);
-       return view('site.contato');
+    public function contato(Request $request) {
+        
+        $motivo_contatos = [
+            "1" => 'Dúvida',
+            "2" => 'Elogio',
+            "3" => 'Reclamação',
+        ];
+        return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request) {
+        // realizar a validação dos dados recebidos na request
+        $request->validate([
+            'nome' => 'required|min:3|max:40', // nomes com no mínomo 3 caracaters e no máximo 40           
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+        // SiteContato::create($request->all());
     }
 }
