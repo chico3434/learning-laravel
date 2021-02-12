c53a2c4f0de0d109170ca3a38c4b8f14f00cecd8
chico3434 <chicokarkow@hotmail.com>
2021-02-12 18:02:29 -0300

    Alteração no banco de dados

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(SiteContatoSeeder::class);
         $this->call(MotivoContatoSeeder::class);
+        $this->call(SiteContatoSeeder::class);
     }
 }
