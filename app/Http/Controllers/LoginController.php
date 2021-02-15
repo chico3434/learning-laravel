b9c93f8e5d28ce23958739ce81b3acbdf78bc263
chico3434 <chicokarkow@hotmail.com>
2021-02-15 14:27:38 -0300

    Tela de Login sendo implementada

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class LoginController extends Controller
+{
+    public function index(Request $request) {
+        $erro = '';
+        if($request->get('erro') == 1) {
+            $erro = 'Usuário ou senha não existe';
+        }
+        return view('site.login', ['titulo' => 'Login', 'erro' => $erro]);
+    }
+
+    public function autenticar(Request $request) {
+        // regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+
+        $request->validate($regras, $feedback);
+        
+        $email = $request->get('usuario');
+        $password = $request->get('senha'); 
+        
+        $user = new User();
+        $usuario = $user->where('email', $email)->where('password', $password)->get()->first();
+
+        if(isset($usuario->name)) {
+            echo 'Usuário existe';
+        } else {
+            return redirect()->route('site.login', ['erro' => 1]);
+        }
+        print_r($usuario);
+    }
+}
