d53c03662bdd47d5b32a95e0472648a583935c51
chico3434 <chicokarkow@hotmail.com>
2021-02-14 21:38:50 -0300

    Middleware Autenticação criado

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
-Route::prefix('/app')->group(function() {
+Route::middleware('autenticacao')->prefix('/app')->group(function() {
     Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
     Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
     Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
